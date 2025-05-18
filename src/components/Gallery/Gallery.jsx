790295b165909bafb6aade18139565eae1cd78e3
moesha463 <smirnovzena463@gmail.com>
2025-05-18 17:05:50 +0300

    commit

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -49,7 +49,7 @@ const Gallery = () => {
             <div className="flex justify-center mb-12">
                 <div className="bg-white border-6 border-black flex items-center justify-center">
                     <h1 className="text-3xl font-regular uppercase text-center px-4 py-2">
-                        Home
+                        Gallery
                     </h1>
                 </div>
             </div>
