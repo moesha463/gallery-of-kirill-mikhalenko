0823d4498a8b3c82952f9c9d07598c30bdc97359
moesha463 <smirnovzena463@gmail.com>
2025-05-31 03:40:53 +0300

    commit

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD2lSlVOKR4R6P3Im4pyQiu72omupkq39Y",
-    authDomain: "artportfolio-3a450.firebaseapp.com",
-    projectId: "artportfolio-3a450",
-    storageBucket: "artportfolio-3a450.firebasestorage.app",
-    messagingSenderId: "376606642786",
-    appId: "1:376606642786:web:4a33080f8337817dbcf73b",
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
